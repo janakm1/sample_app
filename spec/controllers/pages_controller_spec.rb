0f279b03b7fb3fb4dea07bb01e7d322a6e71b75c
Murali J <jmuralidha@yahoo.com>
2011-03-06 15:00:36 -0700

    finished layout chapter 5

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
     get 'home'
     response.should have_selector("title", 
-                        :content => "My sample app | Home" )
+                        :content => "My Sample App | Home" )
    end
   end
 
@@ -25,7 +25,7 @@ describe PagesController do
     it "should have the right title" do
     get 'contact'
     response.should have_selector("title", 
-                        :content => "My sample app | Contact" )
+                        :content => "My Sample App | Contact" )
    end
   end
 
@@ -38,7 +38,7 @@ describe PagesController do
     it "should have the right title" do
     get 'about'
     response.should have_selector("title", 
-                        :content => "My sample app | About" )
+                        :content => "My Sample App | About" )
    end
   end
 
